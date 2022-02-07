2c41bc72220ed9cda20862510ddb937ffd128db1
Kuroi_Cc <ChenchengMiKuRo@gmail.com>
2022-02-07 16:48:11 +0900

    endpoint talk use recruit api

--- step0/backend/app/main.py
+++ step0/backend/app/main.py
@@ -1,6 +1,25 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Body
+import requests
+
+from app import config
 
 app = FastAPI()
+
+
+@app.post('/talk')
+async def get_talk(query: str = Body(..., embed=True)):
+
+    api_url = 'https://api.a3rt.recruit.co.jp/talk/v1/smalltalk'
+    form = {
+        'apikey': (None, config.RECRUIT_API_KEY),
+        'query': (None, query),
+    }
+
+    res = requests.post(api_url, files=form)
+
+    res_json = res.json()
+
+    return res_json
 
 
 @app.get('/')
