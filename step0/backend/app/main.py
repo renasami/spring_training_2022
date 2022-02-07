49d93df1cc4f008807ea5ad08e700aa6bfd16bb0
Kuroi_Cc <ChenchengMiKuRo@gmail.com>
2022-02-07 16:54:57 +0900

    add cors

--- step0/backend/app/main.py
+++ step0/backend/app/main.py
@@ -1,9 +1,18 @@
 from fastapi import FastAPI, Body
+from fastapi.middleware.cors import CORSMiddleware
 import requests
 
 from app import config
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=['*'],
+    allow_credentials=True,
+    allow_methods=['*'],
+    allow_headers=['*'],
+)
 
 
 @app.post('/talk')
