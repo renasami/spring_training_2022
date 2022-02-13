496d35584afa00666ef1177b4bd0731b2ba7e338
Kuroi_Cc <ChenchengMiKuRo@gmail.com>
2022-02-13 21:59:52 +0900

    これだけでオープンチャットができる！！

--- step2/backend/app/main.py
+++ step2/backend/app/main.py
@@ -1,10 +1,14 @@
-from fastapi import FastAPI, Body
+import logging
+from typing import List
+
+from fastapi import FastAPI, Body, WebSocket, WebSocketDisconnect
 from fastapi.middleware.cors import CORSMiddleware
 import requests
 
 from app import config
 
 app = FastAPI()
+logging.basicConfig(level=logging.DEBUG)
 
 app.add_middleware(
     CORSMiddleware,
@@ -13,6 +17,36 @@ app.add_middleware(
     allow_methods=['*'],
     allow_headers=['*'],
 )
+
+active_ws_connections: List[WebSocket] = []
+
+
+@app.websocket('/chat')
+async def chat(websocket: WebSocket):
+    await websocket.accept()
+    logging.getLogger("fastapi").debug("    WebSocket connection accepted")
+    # 接続中のclientを保持
+    active_ws_connections.append(websocket)
+
+    # headersの中のnicknameを取得
+    # ない場合はunknown_{ipアドレス}にする
+    if 'nickname' in websocket.headers.keys():
+        nickname = websocket.headers.get('nickname')
+    else:
+        nickname = f'unknown_{websocket.client.host}'
+
+    logging.getLogger("fastapi").debug(f"   nickname: {nickname}")
+    try:
+        while True:
+            data = await websocket.receive_json()
+            data['nickname'] = nickname
+            logging.getLogger("fastapi").debug(f"   data: {data}")
+            # 全てのclientに送信
+            # 形は{ "nickname": "nickname",　"message": "contents" }
+            for connection in active_ws_connections:
+                await connection.send_json(data)
+    except WebSocketDisconnect:
+        active_ws_connections.remove(websocket)
 
 
 @app.post('/talk')
