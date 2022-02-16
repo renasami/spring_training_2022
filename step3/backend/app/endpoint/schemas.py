7faeb6377a01258db959d501ecfe68f7b2c31e5d
Kuroi_Cc <ChenchengMiKuRo@gmail.com>
2022-02-16 18:04:16 +0900

    login endpoint

---
+++ step3/backend/app/endpoint/schemas.py
@@ -0,0 +1,30 @@
+from typing import List
+
+from pydantic import BaseModel
+
+
+class User(BaseModel):
+    id: int
+    username: str
+
+    class Config:
+        orm_mode = True
+
+
+class Group(BaseModel):
+    id: int
+    name: str
+    members: List[User]
+
+    class Config:
+        orm_mode = True
+
+
+class LoginUser(BaseModel):
+    id: int
+    username: str
+    friends: List[User]
+    groups: List[Group]
+
+    class Config:
+        orm_mode = True
