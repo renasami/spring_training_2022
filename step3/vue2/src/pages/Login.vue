3259c30e8e07a1673567e9e9e2a3baaba5071104
renasami <drrr0502@gmail.com>
2022-02-16 21:15:08 +0900

    add login

--- step3/vue2/src/pages/Login.vue
+++ step3/vue2/src/pages/Login.vue
@@ -1,70 +1,98 @@
 <template>
-     <v-app id="inspire">
-      <v-content>
-         <v-container fluid fill-height>
-            <v-layout align-center justify-center>
-               <v-flex xs12 sm8 md4>
-                  <v-card class="elevation-12">
-                     <v-toolbar dark color="primary">
-                        <v-toolbar-title>Login form</v-toolbar-title>
-                     </v-toolbar>
-                     <v-card-text>
-                        <v-form>
-                           <v-text-field
-                              id="name"
-                              prepend-icon="mdi-account-circle"
-                              v-model="name"
-                              name="naem"
-                              label="name"
-                              type="text"
-                           ></v-text-field>
-                           <v-text-field
-                              id="pas"
-                              prepend-icon="mdi-key"
-                              v-model="password"
-                              label="Password"
-                              type="password"
-                           ></v-text-field>
-                        </v-form>
-                     </v-card-text>
-                     <v-card-actions>
-                        <v-spacer></v-spacer>
-                        <v-btn color="primary" @click="login">
-                        <v-icon left>mdi-login</v-icon>
-                        Login
-                        </v-btn>
-                     </v-card-actions>
-                  </v-card>
-               </v-flex>
-            </v-layout>
-         </v-container>
-      </v-content>
-   </v-app>
+  <v-app id="inspire">
+    <v-content>
+      <v-container fluid fill-height>
+        <v-layout align-center justify-center>
+          <v-flex xs12 sm8 md4>
+            <v-card class="elevation-12">
+              <v-toolbar dark color="primary">
+                <v-toolbar-title>Login form</v-toolbar-title>
+              </v-toolbar>
+              <v-card-text>
+                <v-form>
+                  <v-text-field
+                    id="name"
+                    prepend-icon="mdi-account-circle"
+                    v-model="name"
+                    name="naem"
+                    label="name"
+                    type="text"
+                  ></v-text-field>
+                  <v-text-field
+                    id="pas"
+                    prepend-icon="mdi-key"
+                    v-model="password"
+                    label="Password"
+                    type="password"
+                  ></v-text-field>
+                </v-form>
+              </v-card-text>
+              <v-card-actions>
+                <v-spacer></v-spacer>
+                <v-btn color="primary" @click="login">
+                  <v-icon left>mdi-login</v-icon>
+                  Login
+                </v-btn>
+              </v-card-actions>
+            </v-card>
+          </v-flex>
+        </v-layout>
+        <v-dialog v-model="error" persistent width="360">
+          <v-card align="center">
+            <v-card-title> ユーザー名もしくはパスワードが違います </v-card-title>
+            <v-card-text>
+              もう一度やり直してください
+            </v-card-text>
+            <v-btn @click="retry" outlined color="red lighten-2" class="mb-3"
+              >OK</v-btn
+            >
+          </v-card>
+        </v-dialog>
+      </v-container>
+    </v-content>
+  </v-app>
 </template>
 
 <script lang="ts">
-type Data= {
-    name:string
-    password:string
-}
-import Vue from 'vue'
+type Data = {
+  name: string;
+  password: string;
+  error: boolean;
+};
+import Vue from "vue";
 export default Vue.extend({
-    name: "Login",
-    data():Data {
-        return{
-        name: "",
-        password: "",
-        }
-    },
+  name: "Login",
+  data(): Data {
+    return {
+      name: "",
+      password: "",
+      error: false
+    };
+  },
   props: {
     source: String,
   },
-  methods:{
-      login():void {
-          const name = this.name
-          const pass = this.password
-          console.log(name, pass)
-      }
-  }
-})
+  methods: {
+    login: async function (): Promise<void> {
+      if (this.name === "" || this.password === "") return;
+      if (!this.validLanguage(this.name)) return;
+      const key = btoa(`${this.name}:${this.password}`);
+      const headers = {
+        Authorization: `Basic ${key}`,
+        accept: "application/json",
+      };
+      const url = "http://localhost:8080/user/login"
+      const result = await fetch(url,{headers});
+      console.log(result)
+      if (result.status === 401) this.error = true;
+    },
+     validLanguage: function (str:string): boolean {
+      return ( str.match(/^[A-Za-z0-9]*$/) )? true : false
+    },
+    retry: function (): void {
+      console.log("fa");
+      this.error = false;
+    },
+  },
+});
 </script>
