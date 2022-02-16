2eaa6d82031db5c8483ec86b80c33574fabbf110
Kuroi_Cc <ChenchengMiKuRo@gmail.com>
2022-02-16 18:50:09 +0900

    register endpoint

--- step3/backend/app/endpoint/user.py
+++ step3/backend/app/endpoint/user.py
@@ -4,12 +4,18 @@ from sqlalchemy.orm import Session
 
 from app.db import crud
 from app.db.base import get_db
-from app.endpoint.schemas import LoginUser
+from app.endpoint.schemas import LoginUser, User, UserCreate
 from app.security import auth
 
 router = APIRouter()
 
 security = HTTPBasic()
+
+
+@router.post('/register', response_model=User)
+def register(user_in: UserCreate, db: Session = Depends(get_db)):
+    user = crud.user.create(db, user=user_in)
+    return user
 
 
 @router.get('/login', response_model=LoginUser)
