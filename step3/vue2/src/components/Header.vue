7ce88754474e62659a23dc3f0e14a5a5756e0579
renasami <drrr0502@gmail.com>
2022-02-16 14:02:27 +0900

    一旦commit

---
+++ step3/vue2/src/components/Header.vue
@@ -0,0 +1,36 @@
+<template>
+
+    <v-app-bar app color="primary" dark>
+      <div class="d-flex align-center">
+        <v-img
+          alt="Vuetify Logo"
+          class="shrink mr-2"
+          contain
+          src="https://cdn.vuetifyjs.com/images/logos/vuetify-logo-dark.png"
+          transition="scale-transition"
+          width="40"
+        />
+      </div>
+
+      <v-spacer></v-spacer>
+
+      <v-btn
+        href="/"
+        target="_blank"
+        text
+      >
+        <span class="mr-2">Log Out</span>
+        <v-icon>mdi-open-in-new</v-icon>
+      </v-btn>
+    </v-app-bar>
+    
+</template>
+<script lang="ts">
+import Vue from 'vue'
+export default Vue.extend({
+    name:"Header"
+})
+</script>
+<style scoped>
+
+</style>
