43eec33bd581d320be0d0989f3d01c55fb39d962
renasami <drrr0502@gmail.com>
2022-02-21 02:09:53 +0900

    送信用UI追加

--- step3/vue2/src/pages/Chat.vue
+++ step3/vue2/src/pages/Chat.vue
@@ -1,9 +1,13 @@
 <template>
     <div id="chat-space">
         <ul v-for="talk in talks" :key="talk.id">
-            <p>{{ talk }}</p>
-            <p>id: {{ this }}</p>
+            <!-- <p>{{ talk }}</p> -->
+            <p>{{ talk.sender_id == myId ? myName : selectedFriend.username }}:{{ talk.message }}</p>
         </ul>
+        <div id="input-space">
+        <v-text-field></v-text-field>
+        <v-btn>送信</v-btn>
+        </div>
     </div>
 </template>
 <script lang="ts">
@@ -14,14 +18,19 @@ export default Vue.extend({
     name: 'Chat',
     data() {
         return{
-        talks: []
+        talks: [],
+        myName:"",
+        myId: 0,
+        selectedFriend: null,
         }
     },
     beforeMount(){
         this.$store.subscribe(async (mutation,state) =>{
-        if (mutation.type === 'updateIndex') {
+        if (mutation.type == 'updateIndex') {
             const selectedFriend = state.friends[state.index];
-            console.log(selectedFriend)
+            this.myName = this.$store.state.name
+            this.selectedFriend = selectedFriend
+            this.myId = this.$store.state.id
             const headers = {
             Authorization: state.token,
             accept: "application/json",
@@ -40,5 +49,12 @@ export default Vue.extend({
     /* margin-left: 25vw; */
     width: 70vw;
     height: 100vh;
+}
+#input-space {
+    display: flex;
+    position: absolute;
+    width: 65vw;
+    bottom: 10vh;
+    /* background-color:green; */
 }
 </style>
